0f8de094304d183e61ef6f74b527894aea240cd8
alvinirwanto <irwantoalvin@gmail.com>
2022-12-01 11:41:37 +0700

    add margin top in mobile version

--- src/components/TrustedBy.jsx
+++ src/components/TrustedBy.jsx
@@ -7,9 +7,9 @@ import MashableLogo from '../assets/images/logo__mashable.svg'
 
 const TrustedBy = () => {
     return (
-        <div className='bg-[#F4F6F6] mt-5 w-full py-[4rem] my-[4rem] md:my-0 flex justify-center px-4'>
+        <div className='bg-[#F4F6F6] mt-[10rem] md:mt-5 w-full py-[4rem] my-[4rem] md:my-0 flex justify-center px-4'>
             <div className='w-full'>
-                <h1 data-aos="fade-up" data-aos-duration="500" className='capitalize text-center font-poppins font-semibold text-xl tracking-wider pb-9'>TRUSTED BY OVER 1K+ COMPANIES</h1>
+                <h1 data-aos="fade-up" data-aos-duration="500" className='capitalize text-center break-words font-poppins font-semibold text-xl tracking-wider pb-9'>TRUSTED BY OVER 1K+ COMPANIES</h1>
                 <div className='flex justify-center flex-wrap md:gap-[3rem] gap-[3rem] xl:gap-[9rem] px-4 mx-auto'>
                     <img data-aos="fade-up" data-aos-duration="1000" src={MicrosoftLogo} alt="" className='w-[7rem]' />
                     <img data-aos="fade-up" data-aos-duration="1300" src={EntrepreneurLogo} alt="" className='w-[7rem]' />
